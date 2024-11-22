fa324db4f468ac376acc3dad29d9e08f927a5937
hyh <hyhping@hyhdeMacBook-Air.local>
2024-11-22 19:15:41 +0800

    finish all functions: beta

--- predict.py
+++ predict.py
@@ -30,7 +30,7 @@ class Dataloader(Dataset):
             self.offset = 0
         else:
             self.offset = offset
-        self.df = pd.read_csv(csv_file, header=None, skiprows=1)[0].tolist()
+        self.df = pd.read_csv(csv_file, header=None, skiprows=1)[0].tolist()[:10]
         if "trainval" in csv_file.lower():
             self.df = pd.read_csv(csv_file, header=None, skiprows=1)
             self.dataframe = self.df.groupby(1).apply(lambda x: x.sample(sample)).reset_index(drop=True)
@@ -41,7 +41,7 @@ class Dataloader(Dataset):
             self.mode = 0
             if existed is not None:
                 dfe = pd.read_csv(existed, header=None, skiprows=1)
-                retry = dfe[0][dfe.where(self.dfe[1] == -1).dropna().index]
+                retry = dfe[0][dfe.where(dfe[1] == -1).dropna().index]
                 miss = set(self.df) - set(dfe[0].tolist())
                 self.df = retry.tolist() + list(miss)
         self.csv_file = csv_file
@@ -73,8 +73,11 @@ def png2base64(img_path, size=(224,224)):
     
 def getIndex(text:str):
     index = -1
-    if text.find("[[[") != -1 and text.find("]]]") != -1:
-        index = int(text[text.find("[[["):text.find("]]]")])
+    try :
+        if text.find("[[[") != -1 and text.find("]]]") != -1:
+            index = int(text[text.find("[[[")+3:text.find("]]]")])
+    except:
+        pass
     return index
 
 def request(url, name:str, data_list:list, prompt, model, size=(224, 224),
@@ -127,7 +130,7 @@ def request(url, name:str, data_list:list, prompt, model, size=(224, 224),
         finally:
             response.close()
     else:
-        logging.warning("failed to get response. Name: {}. Status code: {}".format(name, response.status_code), end='\r')
+        logging.warning("failed to get response. Name: {}. Status code: {}".format(name, response.status_code))
         return name, ""
 
     return name, output
@@ -144,7 +147,7 @@ class Client:
         self.size = size
     
     def __call__(self, ):
-        return self.url, self.categories, self.prompt, self.model, self.size
+        return self.url, self.categories, self.prompt, self.model, self.size, self.worker_num
 
     def start(self, dataloader: DataLoader):
         for name, data in tqdm(dataloader):
@@ -161,12 +164,18 @@ class Client:
             results[name] = result
         return results
 
-    def save_result(self, results, save_dir='output.csv', csv_file='./test_for_student.csv'):
-        temp = Dataloader(csv_file=csv_file, frames = 6, root='hw3_16fpv')
-        with open(save_dir, "w") as f:
-            f.writelines("Id,Category\n")
-            for name in temp.df:
-                f.writelines(f"{name},{results[name]}\n")
+    def save_result(self, results, save_dir='output.csv', csv_file=None):
+        if csv_file is not None:  # For normal prediction
+            temp = Dataloader(csv_file=csv_file, frames = 6, root='hw3_16fpv')
+            with open(save_dir, "w") as f:
+                f.writelines("Id,Category\n")
+                for name in temp.df:
+                    f.writelines(f"{name},{results[name]}\n")
+        else:  # for fixing missed results
+            with open(save_dir, "w") as f:
+                f.writelines("Id,Category\n")
+                for name, result in results.items():
+                    f.writelines(f"{name},{result}\n")
 
 def makePrompt(categories:dict):
     prompt = '''
@@ -177,51 +186,64 @@ def makePrompt(categories:dict):
 
     return prompt
 
-def fixMissed(output:str, oldDataloader: DataLoader, oldClient: Client, max_retries = 10):
+def intergrate(tempFile, outputFile):
+    dft = pd.read_csv(tempFile, header=None, skiprows=1)
+    dfo = pd.read_csv(outputFile, header=None, skiprows=1)
+    for i in range(len(dft)):
+        try:
+            if dfo.loc[dfo[0] == dft.loc[i, 0], 1].item() < 0:
+                dfo.loc[dfo[0] == dft.loc[i, 0], 1] = dft.loc[i, 1]
+        except:
+            logging.warning(f"failed to find {dft.iloc[i, 0]}")
+    with open(outputFile, "w") as f:
+        f.writelines("Id,Category\n")
+        for i in range(len(dfo)):
+            f.writelines(f"{dfo.loc[i, 0]},{dfo.loc[i, 1]}\n")
+
+
+def fixMissed(output:str, oldDataloader: DataLoader, oldClient: Client, max_retries = 5):
     csv_file, root, frames, offset = oldDataloader()
-    url, categories, prompt, model, size = oldClient()
+    url, categories, prompt, model, size, worker_num = oldClient()
 
-    tempFile = "temp.csv"
-    finalOutput = output.replace(".csv", "_final.csv")
+    tempFile = "temp.csv"  # used to store new generated results
+    finalOutput = output.replace(".csv", "_final.csv")  # used to store the final results
 
-    if not os.path.exists(tempFile):
-        os.system(f"cp {output} {tempFile}")
-    else:
-        os.system(f"rm {tempFile}")
-        os.system(f"cp {output} {tempFile}")
+    if os.path.exists(finalOutput):
+        os.system(f"rm {finalOutput}")
+    os.system(f"cp {output} {finalOutput}")
 
-    temp = Dataloader(csv_file=csv_file, frames = frames, root = root, offset = offset, existed = tempFile)
+    temp = Dataloader(csv_file=csv_file, frames = frames, root = root, offset = offset, existed = finalOutput)
     tries = 1
     while temp.df !=0 and tries <= max_retries: 
         logging.info("Retrying for {}th time. Retries left: {}".format(tries, max_retries-tries))
         if max_retries - tries > 4: # more than 4 retries left
-            temp = Dataloader(csv_file=csv_file, frames = frames, root = root, offset = offset, existed = output)
-            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=args.worker_num, model=args.model, size=args.size)
+            temp = Dataloader(csv_file=csv_file, frames = frames, root = root, offset = offset, existed = finalOutput)
+            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=worker_num, model=model, size=size)
             results = client.start(temp)
-            client.save_result(results, save_dir=tempFile, csv_file=csv_file)
+            client.save_result(results, save_dir=tempFile)
         elif max_retries - tries > 1: # 2-4 retries left
-            temp = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = tempFile)
-            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=args.worker_num, model=args.model, size=args.size)
+            temp = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = finalOutput)
+            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=worker_num, model=model, size=size)
             results = client.start(temp)
-            client.save_result(results, save_dir=tempFile, csv_file=csv_file)
+            client.save_result(results, save_dir=tempFile)
         else: # less than 2 retries left
-            temp = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = tempFile)
-            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=args.worker_num, model="llava:13b", size=args.size)
+            temp = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = finalOutput)
+            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=worker_num, model="llava:13b", size=size)
             results = client.start(temp)
-            client.save_result(results, save_dir=tempFile, csv_file=csv_file)
+            client.save_result(results, save_dir=tempFile)
+            intergrate(tempFile, finalOutput)
 
-            temp = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = tempFile)
-            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=args.worker_num, model="llama3.2-vision", size=args.size)
+            temp = Dataloader(csv_file=csv_file, frames = 1, root = root, offset = 0, existed = finalOutput)
+            client = Client(url=url, categories=categories, prompt=makePrompt(categories), worker_num=worker_num, model="llama3.2-vision", size=size)
             results = client.start(temp)
-            client.save_result(results, save_dir=tempFile, csv_file=csv_file)
-            
+            client.save_result(results, save_dir=tempFile)
+        intergrate(tempFile, finalOutput)
         tries += 1
-    final = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = tempFile)
+    final = Dataloader(csv_file=csv_file, frames = 16, root = root, offset = 0, existed = finalOutput)
     if len(final.df) > 0:
-        logging.info("Retrying for {} times. We can not fix all the errors".format(max_retries))
+        print("Retrying for {} times. We can not fix all the errors".format(max_retries))
     else:
-        logging.info("Retrying for {} times. We can fix all the errors".format(tries))
-    os.system(f"cp {tempFile} {finalOutput}")
+        print("Retrying for {} times. We can fix all the errors".format(tries))
 
 
 categories  = {
@@ -251,14 +273,16 @@ if __name__ == "__main__":
     parser.add_argument("--size", type=int, nargs=2, default=(224, 224))
     parser.add_argument("--offset", default=0, type=int)
     parser.add_argument("--fix", action="store_true")
+    parser.add_argument("--only-fix", action="store_true")
     args = parser.parse_args()
 
     prompt = makePrompt(categories)
 
     dataloader = Dataloader(csv_file=args.csv_file, frames=args.frames, root=args.root, offset=args.offset)
     client = Client(url.format(args.url), categories, prompt, worker_num=args.worker_num, model=args.model, size=args.size)
-    results = client.start(dataloader)
-    client.save_result(results, args.save_dir, args.csv_file)
+    if not args.only_fix:
+        results = client.start(dataloader)
+        client.save_result(results, args.save_dir, args.csv_file)
 
-    if args.fix:
+    if args.fix or args.only_fix:
         fixMissed(args.save_dir, dataloader, client)
